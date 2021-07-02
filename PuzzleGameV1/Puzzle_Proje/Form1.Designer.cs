7e572fb7ec9858e837c3c601c8e4ec19bd9489d3
SalihSalurr <58612345+SalihSalurr@users.noreply.github.com>
2021-07-03 01:00:46 +0300

    v1

---
+++ PuzzleGameV1/Puzzle_Proje/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace Puzzle_Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PuzzleTable = new System.Windows.Forms.GroupBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MoveCount = new System.Windows.Forms.Label();
+            this.MixButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.PuzzleTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PuzzleTable
+            // 
+            this.PuzzleTable.BackColor = System.Drawing.Color.Transparent;
+            this.PuzzleTable.Controls.Add(this.pictureBox9);
+            this.PuzzleTable.Controls.Add(this.pictureBox8);
+            this.PuzzleTable.Controls.Add(this.pictureBox7);
+            this.PuzzleTable.Controls.Add(this.pictureBox6);
+            this.PuzzleTable.Controls.Add(this.pictureBox5);
+            this.PuzzleTable.Controls.Add(this.pictureBox4);
+            this.PuzzleTable.Controls.Add(this.pictureBox3);
+            this.PuzzleTable.Controls.Add(this.pictureBox2);
+            this.PuzzleTable.Controls.Add(this.pictureBox1);
+            this.PuzzleTable.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.PuzzleTable.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PuzzleTable.Location = new System.Drawing.Point(221, 51);
+            this.PuzzleTable.Name = "PuzzleTable";
+            this.PuzzleTable.Size = new System.Drawing.Size(446, 442);
+            this.PuzzleTable.TabIndex = 0;
+            this.PuzzleTable.TabStop = false;
+            this.PuzzleTable.Text = "Puzzle";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(293, 291);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(157, 291);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(21, 291);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(293, 155);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(157, 155);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(21, 155);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(293, 19);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(157, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.ChangePictureBox);
+            // 
+            // MoveCount
+            // 
+            this.MoveCount.AutoSize = true;
+            this.MoveCount.BackColor = System.Drawing.Color.Transparent;
+            this.MoveCount.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MoveCount.ForeColor = System.Drawing.Color.White;
+            this.MoveCount.Location = new System.Drawing.Point(211, 519);
+            this.MoveCount.Name = "MoveCount";
+            this.MoveCount.Size = new System.Drawing.Size(311, 55);
+            this.MoveCount.TabIndex = 5;
+            this.MoveCount.Text = "Hamle Sayısı : ";
+            // 
+            // MixButton
+            // 
+            this.MixButton.BackColor = System.Drawing.Color.Black;
+            this.MixButton.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MixButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.MixButton.Location = new System.Drawing.Point(857, 342);
+            this.MixButton.Name = "MixButton";
+            this.MixButton.Size = new System.Drawing.Size(139, 52);
+            this.MixButton.TabIndex = 7;
+            this.MixButton.Text = "Karıştır";
+            this.MixButton.UseVisualStyleBackColor = false;
+            this.MixButton.Click += new System.EventHandler(this.MixButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.BackColor = System.Drawing.Color.Black;
+            this.QuitButton.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.QuitButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.QuitButton.Location = new System.Drawing.Point(857, 448);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(139, 45);
+            this.QuitButton.TabIndex = 8;
+            this.QuitButton.Text = "Çıkış";
+            this.QuitButton.UseVisualStyleBackColor = false;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::Puzzle_Proje.Properties.Resources.orginal_picture;
+            this.pictureBox10.Location = new System.Drawing.Point(756, 51);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(240, 240);
+            this.pictureBox10.TabIndex = 10;
+            this.pictureBox10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Puzzle_Proje.Properties.Resources.pexels_pixabay_220201;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1164, 631);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.MixButton);
+            this.Controls.Add(this.MoveCount);
+            this.Controls.Add(this.PuzzleTable);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.PuzzleTemplate_Load);
+            this.PuzzleTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox PuzzleTable;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label MoveCount;
+        private System.Windows.Forms.Button MixButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
---
+++ PuzzleGameV1/Puzzle_Proje/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle_Proje
+{
+    public partial class Form1 : Form
+    {
+        int NullIndex, MoveIndex = 0;   //Boş picturebox ve hareket eden picturebox indexleri
+        List<Bitmap> PictureList = new List<Bitmap>();   //Resimleri tutmak için liste. Bitmap sınıfı görseller üzerinde çalışabilmek için 
+        int minute = 0, second = 0, splitsecond = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PictureList.AddRange(new Bitmap[] { Properties.Resources.Picture0, Properties.Resources.Picture1, Properties.Resources.Picture2,
+                Properties.Resources.Picture3, Properties.Resources.Picture4,Properties.Resources.Picture5,Properties.Resources.Picture6,
+                Properties.Resources.Picture7,Properties.Resources.Picture8, Properties.Resources._null}); // Resimleri tuttuğumuz listeye resimleri ekliyoruz
+            MoveCount.Text += MoveIndex; // Hareket sayısını sayacak
+        }
+        private void PuzzleTemplate_Load(object sender, EventArgs e)
+        {
+            Mixing();   //Karıştırma işlemi
+        }
+
+        void Mixing()
+        {
+            do
+            {
+                int indx;   // Resimlerin rastgele gideceği index
+                Random rnd = new Random();
+                List<int> Indexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });  // 8 numaralı index son fotoğraf olduğu için yok
+                for (int i = 0; i < 9; i++)
+                {
+                    Indexes.Remove((indx = Indexes[rnd.Next(0, Indexes.Count)]));  //Lisedeki i indexi kaldırıyor ki alt satırda resimleri rasgele yerleştirsin 
+                    ((PictureBox)PuzzleTable.Controls[i]).Image = PictureList[indx];
+                    if (indx == 9)
+                        NullIndex = i; //boş resim
+                }
+            } while (CheckWin());
+        }
+
+        private void MixButton_Click(object sender, EventArgs e)
+        {
+            Mixing();
+            MoveIndex = 0;
+            MoveCount.Text = "Hamle Sayısı : 0";
+        }
+
+        // Kontrol metodu
+        bool CheckWin()
+        {
+            int i;
+            for (i = 0; i < 8; i++)  //Eğer sekiz fotoğrafın sekizi de doğru yerinde mi kontrol ettiriliyor.
+            {
+                if ((PuzzleTable.Controls[i] as PictureBox).Image != PictureList[i]) break;
+            }
+            if (i == 8)
+                return true;
+            else
+                return false;
+        }
+
+        // Çıkış butonu
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Quite(sender, e as FormClosingEventArgs);
+        }
+
+        // Çıkış penceresi 
+        private void Quite(object sender, FormClosingEventArgs e)
+        {
+            DialogResult YN = MessageBox.Show("Çıkmak istediğinize emin misiniz?", "Roket Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sender as Button != QuitButton && YN == DialogResult.No)
+                e.Cancel = true;
+            if (sender as Button == QuitButton && YN == DialogResult.Yes)
+                Environment.Exit(0);
+        }
+
+        //Fotoğrafların değişimi sağlayan metot 
+        private void ChangePictureBox(object sender, EventArgs e)
+        {
+            int PictureBoxIndex = PuzzleTable.Controls.IndexOf(sender as Control);
+            if (NullIndex != PictureBoxIndex)
+            {
+                List<int> ContactBoxes = new List<int>(new int[] { ((PictureBoxIndex %3 == 0) ? - 1 : PictureBoxIndex - 1), PictureBoxIndex - 3,
+                (PictureBoxIndex % 3 == 2) ? - 1 : PictureBoxIndex + 1, PictureBoxIndex + 3});   // Boş picturebox ile bağlantılı picturebox'ların tutulduğu list
+                if (ContactBoxes.Contains(NullIndex))  // Bağlantılardan biri null picturebox ile bağlantılıysa 
+                {
+                    ((PictureBox)PuzzleTable.Controls[NullIndex]).Image = ((PictureBox)PuzzleTable.Controls[PictureBoxIndex]).Image; //Null picturebox ile
+                    ((PictureBox)PuzzleTable.Controls[PictureBoxIndex]).Image = PictureList[9];                                      // doluyu değiştiriyor
+                    NullIndex = PictureBoxIndex;
+                    MoveCount.Text = "Hamle Sayısı : " + (++MoveIndex);
+                    if (CheckWin())      // Eğer parçalar doğru yerleştirildiyse tebrikler ekranı çıkıyor ve puzzle sıfırlanıyor.
+                    {
+                        (PuzzleTable.Controls[8] as PictureBox).Image = PictureList[8];
+                        MessageBox.Show("Tebrikler");
+                        MoveIndex = 0;
+                        MoveCount.Text = "Hamle Sayısı : 0";
+                        Mixing();
+                    }
+                }
+            }
+        }
+    }
+}
